05dd3e594356a9a210d5248c707059958020d846
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-10-22 18:34:50 -0500

    feat: dynamic post URLs.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,9 +1,18 @@
 import { getConfig } from '@/services/config/getConfig';
+import fs from 'fs';
 import { MetadataRoute } from 'next';
+import path from 'path';
 
 export default function robots(): MetadataRoute.Robots {
   const config = getConfig();
   const siteUrl = config.siteUrl;
+
+  // Load posts data from JSON file
+  const filePath = path.join(process.cwd(), 'public', 'postsData.json');
+  const posts = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+  // Generate robots.txt entries for each post
+  const postUrls = posts.map((post) => `/posts/${post.slug}`);
 
   return {
     rules: {
@@ -13,9 +22,10 @@ export default function robots(): MetadataRoute.Robots {
         '/',
         '/about',
         '/friends',
-        '/posts/*',
+        '/posts',
         '/categories/*',
         '/tags/*',
+        ...postUrls, // Dynamic post URLs
       ],
     },
     sitemap: `${siteUrl}/sitemap.xml`,
