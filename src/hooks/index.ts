de9373d2f2ef0c3b79b56d4a90551a5ee9ae5cf8
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-11-13 14:35:17 -0600

    refactor(BackToTop): Move hook out of component
    - Move useVisibilityOnScroll hook out of BackToTop component.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useDebouncedResize } from './useDebouncedResize';
 export { default as useDebouncedScroll } from './useDebouncedScroll';
 export { default as useOutsideClick } from './useOutsideClick';
 export { default as useScrollProgress } from './useScrollProgress';
+export { default as useVisibilityOnScroll } from './useVisibilityOnScroll';
