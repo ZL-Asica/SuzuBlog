2fc5c7e2796addafded6d8f0a04372ca7e089b3e
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-11-13 14:15:19 -0600

    refactor(header/footer): Extract common components and hooks
    - Extract renderMenuItems and renderSocialMediaLink
    - Extract useIsMobile, useOutsideClick, useScrollProgress hooks from Header

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { default as useIsMobile } from './useIsMobile';
+export { default as useDebouncedResize } from './useDebouncedResize';
+export { default as useDebouncedScroll } from './useDebouncedScroll';
+export { default as useOutsideClick } from './useOutsideClick';
+export { default as useScrollProgress } from './useScrollProgress';
