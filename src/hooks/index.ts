202ae94c4e34c4789750f59b5afb86290f88206b
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-11-14 14:13:56 -0600

    refactor(hooks): remove unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,6 @@
-export { default as useIsMobile } from './useIsMobile';
 export { default as useDebouncedResize } from './useDebouncedResize';
 export { default as useDebouncedScroll } from './useDebouncedScroll';
 export { default as useOutsideClick } from './useOutsideClick';
 export { default as useScrollProgress } from './useScrollProgress';
 export { default as useVisibilityOnScroll } from './useVisibilityOnScroll';
-export { default as useTOCPosition } from './useTOCPosition';
 export { default as useTOCLogic } from './useTOCLogic';
-export { default as useFixedPosition } from './useFixedPosition';
