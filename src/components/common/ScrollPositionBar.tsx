440d4f4a7458b1bfc642ee9843da4373f8071e63
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2025-03-01 18:35:06 -0600

    fix: scroll position bar hidden behind

---
+++ src/components/common/ScrollPositionBar.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useScrollPosition } from '@zl-asica/react'
+
+const ScrollPositionBar = () => {
+  const scrollProgress = useScrollPosition(undefined, true)
+  return (
+    <div
+      className="fixed left-0 top-0 z-60 h-[3px] w-full bg-[var(--sakuraPink)] transition-all duration-500 ease-out"
+      style={{ width: `${scrollProgress}%` }}
+      aria-hidden
+    />
+  )
+}
+
+export default ScrollPositionBar
