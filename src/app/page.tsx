f0139f218c8aab4f2f7fbc40834282ae81725bc2
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2025-06-15 15:53:10 -0500

    style(home): minor adjust for home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,22 +19,22 @@ async function Home() {
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
-      <div className="flex max-h-[800px] flex-col items-center justify-center px-6">
-        <div className="relative h-[30vh] w-full bg-cover bg-center md:h-[50vh]">
-          <div className="absolute left-1/2 top-[25%] -translate-x-1/2 transform">
-            <Image
-              src={config.avatar}
-              alt="Avatar"
-              width={180}
-              height={180}
-              className="rounded-full border-4 border-primary-300 shadow-lg dark:border-primary-200"
-              loading="eager"
-            />
-          </div>
-        </div>
-
-        <div className="mt-20 text-center">
-          <p className="text-foreground mb-28 text-3xl font-bold">{config.slogan}</p>
+      <div className="flex flex-col items-center justify-center px-6 pb-12">
+        <div className="mt-[10vh] flex flex-col items-center justify-center">
+          <Image
+            src={config.avatar}
+            alt="Avatar"
+            width={180}
+            height={180}
+            className="rounded-full border-4 border-primary-300 shadow-lg dark:border-primary-200"
+            priority
+          />
+          <p
+            id="site-slogan"
+            className="mt-10 sm:mt-14 md:mt-20 text-center text-balance text-3xl font-semibold text-foreground mb-16"
+          >
+            {config.slogan}
+          </p>
           <SocialMediaLinks socialMedia={config.socialMedia} iconSize={40} />
         </div>
       </div>
