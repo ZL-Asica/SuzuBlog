ad002653b6beb01567d315e6cdf200fbaf9b391a
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2025-08-01 10:57:15 -0500

    chore(deps): bump up dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,10 @@
 import antfu from '@antfu/eslint-config'
-import nextPlugin from '@next/eslint-plugin-next'
 
 export default antfu({
   formatters: true,
-  react: true,
+  nextjs: true,
   typescript: {
     tsconfigPath: 'tsconfig.json',
-  },
-  plugins: {
-    '@next/next': nextPlugin,
   },
   lessOpinionated: true,
   rules: {
